dbc3a29fcdeb1ff90610c443500c7cabf64e23f6
Pedro <freitaas.pedro@gmail.com>
2025-09-30 19:36:46 -0300

    feat: add Platforms (32x32) tileset and update session file references

---
+++ tiled/tilesets/Platforms (32x32).tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Platforms (32x32)" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="Platforms (32x32).png" width="192" height="128"/>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="11"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="11"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="11"/>
+  </objectgroup>
+ </tile>
+</tileset>
