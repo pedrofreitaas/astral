e5bf984cd5129fe7c78c3af699daa1ad511b4007
Pedro <freitaas.pedro@gmail.com>
2025-10-26 16:03:22 -0300

    Add object group to Terrain and Back Wall tileset for improved collision handling

--- tiled/tilesets/Terrain and Back Wall (32x32).tsx
+++ tiled/tilesets/Terrain and Back Wall (32x32).tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Terrain and Back Wall (32x32)" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Terrain and Back Wall (32x32)" tilewidth="32" tileheight="32" tilecount="247" columns="19">
  <image source="Terrain and Back Wall (32x32).png" width="608" height="416"/>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
