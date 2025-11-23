45dc5996b96591c680658bd8836901d80db2cc46
Pedro <freitaas.pedro@gmail.com>
2025-11-22 22:08:23 -0300

    feat: added spear trap

---
+++ src/actors/traps/Spear.cpp
@@ -0,0 +1,128 @@
+#include "Spear.h"
+
+Spear::Spear(Game *game, const Vector2 &position)
+    : Actor(game, 1.f), mTipCollider(nullptr)
+{
+    mTimerComponent = new TimerComponent(this);
+
+    mColliderComponent = new AABBColliderComponent(
+        this, 4, 60, 8, 3, ColliderLayer::SpearBlock);
+
+    mDrawComponent = new DrawAnimatedComponent(
+        this,
+        "../assets/Sprites/Enemies/Traps/Spear/texture.png",
+        "../assets/Sprites/Enemies/Traps/Spear/texture.json",
+        [this](std::string animationName)
+        { AnimationEndCallback(animationName); });
+
+    mRigidBodyComponent = new RigidBodyComponent(
+        this, 1.0f, 0.0f, false);
+
+    mDrawComponent->AddAnimation("idle", {0});
+    mDrawComponent->AddAnimation("spiking", 1, 7);
+
+    mDrawComponent->SetAnimation("idle");
+
+    mTimerComponent->AddTimer(Spear::TRIGGER_COOLDOWN, [this]()
+                              { Trigger(); });
+
+    mTipCollider = new Collider(
+        mGame,
+        this,
+        GetPosition() + Vector2(5, 49),
+        Vector2(7, 10),
+        [this](bool collided, const float minOverlap, AABBColliderComponent *other)
+        {},
+        DismissOn::None,
+        ColliderLayer::SpearTip,
+        {ColliderLayer::SpearBlock},
+        -1.f);
+
+    SetPosition(position);
+}
+
+void Spear::ManageState()
+{
+    switch (mBehaviorState)
+    {
+    case BehaviorState::Idle:
+        break;
+    case BehaviorState::Attacking:
+        break;
+    default:
+        mBehaviorState = BehaviorState::Idle;
+        break;
+    }
+}
+
+void Spear::AnimationEndCallback(std::string animationName)
+{
+    if (animationName == "spiking")
+    {
+        mBehaviorState = BehaviorState::Idle;
+        mTimerComponent->AddTimer(Spear::TRIGGER_COOLDOWN, [this]()
+                                  { Trigger(); });
+    }
+}
+
+void Spear::ManageAnimations()
+{
+    switch (mBehaviorState)
+    {
+    case BehaviorState::Idle:
+        mDrawComponent->SetAnimation("idle");
+        mDrawComponent->SetAnimFPS(1.f);
+
+        mTipCollider->SetPosition(GetPosition() + Vector2(5, 49));
+
+        break;
+    case BehaviorState::Attacking:
+        mDrawComponent->SetAnimation("spiking");
+        mDrawComponent->SetAnimFPS(7.f);
+
+        switch (mDrawComponent->GetCurrentSprite()) // seven frame animation
+        {
+        case 0:
+            mTipCollider->SetPosition(GetPosition() + Vector2(5, 49));
+            break;
+        case 1:
+            mTipCollider->SetPosition(GetPosition() + Vector2(5, 2));
+            break;
+        case 5:
+            mTipCollider->SetPosition(GetPosition() + Vector2(5, 4));
+            break;
+        case 6:
+            mTipCollider->SetPosition(GetPosition() + Vector2(5, 22));
+            break;
+        default:
+            break;
+        }
+
+        break;
+    default:
+        mDrawComponent->SetAnimation("idle");
+        mDrawComponent->SetAnimFPS(1.f);
+        break;
+    }
+}
+
+void Spear::OnUpdate(float deltaTime)
+{
+    ManageState();
+    ManageAnimations();
+}
+
+void Spear::Trigger()
+{
+    if (mBehaviorState == BehaviorState::Attacking)
+    {
+        return;
+    }
+
+    mBehaviorState = BehaviorState::Attacking;
+}
+
+Vector2 Spear::GetTipCenter() const
+{
+    return mTipCollider->GetCenter();
+}
---
+++ src/actors/traps/Spear.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SDL.h>
+#include "../Actor.h"
+#include "../../components/TimerComponent.h"
+#include "../../components/collider/AABBColliderComponent.h"
+#include "../../components/draw/DrawAnimatedComponent.h"
+#include "../../components/RigidBodyComponent.h"
+#include "../Collider.h"
+
+class Spear : public Actor
+{
+public:
+    const float TRIGGER_COOLDOWN = 2.f;
+
+    explicit Spear(Game* game, const Vector2& position);
+
+    void ManageState();
+    void AnimationEndCallback(std::string animationName);
+    void ManageAnimations();
+    void OnUpdate(float deltaTime) override;
+    void Trigger();
+    Vector2 GetTipCenter() const;
+
+private:
+    TimerComponent *mTimerComponent;
+    AABBColliderComponent *mColliderComponent;
+    DrawAnimatedComponent *mDrawComponent;
+    RigidBodyComponent *mRigidBodyComponent; //just to check the collision
+
+    Collider *mTipCollider;
+};
