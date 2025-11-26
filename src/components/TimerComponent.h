fa5a11e5d5c4c268a94c6cd65db78b954fc3c0e4
Pedro <freitaas.pedro@gmail.com>
2025-11-26 17:31:24 -0300

    feat: refactor TimerComponent to use Timer class for improved timer management

--- src/components/TimerComponent.h
+++ src/components/TimerComponent.h
@@ -3,39 +3,69 @@
 #include <functional>
 #include "./Component.h"
 
+class Timer {
+public:
+    int id;
+    float duration, elapsed;
+    std::function<void()> callback;
+
+    Timer() : id(0), duration(0.f), elapsed(0.f), callback(nullptr) {}
+    Timer(int id, float dur, std::function<void()> cb) : id(id), duration(dur), elapsed(0.f), callback(cb) {}
+};
+
 class TimerComponent : public Component
 {
+private:
+    int nextTimerId = 0;
+
 public:
     TimerComponent(class Actor* owner) : Component(owner) {}
     
     void Update(float deltaTime) override {
         Component::Update(deltaTime);
         
-        for (size_t i = 0; i < mDurations.size(); ++i) {
-            mDurations[i] -= deltaTime;
+        for (size_t i = 0; i < mTimers.size(); ++i) {
+            mTimers[i]->elapsed += deltaTime;
             
-            if (mDurations[i] <= 0.f) {
-                if (mCallbacks[i]) {
-                    mCallbacks[i]();
+            if (mTimers[i]->elapsed >= mTimers[i]->duration) {
+                if (mTimers[i]->callback) {
+                    mTimers[i]->callback();
                 }
             }
         }
 
         // Remove expired timers
-        for (int i = static_cast<int>(mDurations.size()) - 1; i >= 0; --i) {
-            if (mDurations[i] <= 0.f) {
-                mDurations.erase(mDurations.begin() + i);
-                mCallbacks.erase(mCallbacks.begin() + i);
+        for (int i = static_cast<int>(mTimers.size()) - 1; i >= 0; --i) {
+            if (mTimers[i]->elapsed >= mTimers[i]->duration) {
+                delete mTimers[i];
+                mTimers.erase(mTimers.begin() + i);
             }
         }
     }
 
-    void AddTimer(float duration, std::function<void()> callback) {
-        mDurations.push_back(duration);
-        mCallbacks.push_back(callback);
+    Timer* AddTimer(float duration, std::function<void()> callback) {
+        struct Timer* newTimer = new Timer(
+            nextTimerId,
+            duration,
+            callback
+        );
+
+        nextTimerId++;
+
+        mTimers.push_back(newTimer);
+
+        return newTimer;
+    }
+
+    float checkTimerRemaining(Timer* timer) {
+        for (const auto& t : mTimers) {
+            if (t->id == timer->id) {
+                return t->duration - t->elapsed;
+            }
+        }
+        return 0.f;
     }
 
 protected:
-    std::vector<float> mDurations;
-    std::vector<std::function<void()>> mCallbacks;
+    std::vector<Timer*> mTimers;
 };
