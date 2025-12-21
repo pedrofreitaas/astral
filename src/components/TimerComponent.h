ca22298cd9b7a7e8f19157bef21e2d35cf0cc05c
pedromeerkat <pedro@meerkatcoding.com>
2025-12-21 18:29:52 -0300

    feat: enhance Timer functionality with removable state and update logic

--- src/components/TimerComponent.h
+++ src/components/TimerComponent.h
@@ -7,10 +7,11 @@ class Timer {
 public:
     int id;
     float duration, elapsed;
+    bool removable;
     std::function<void()> callback;
 
-    Timer() : id(0), duration(0.f), elapsed(0.f), callback(nullptr) {}
-    Timer(int id, float dur, std::function<void()> cb) : id(id), duration(dur), elapsed(0.f), callback(cb) {}
+    Timer() : id(0), duration(0.f), elapsed(0.f), removable(false), callback(nullptr) {}
+    Timer(int id, float dur, bool removable, std::function<void()> cb) : id(id), duration(dur), elapsed(0.f), removable(removable), callback(cb) {}
 };
 
 class TimerComponent : public Component
@@ -36,7 +37,7 @@ public:
 
         // Remove expired timers
         for (int i = static_cast<int>(mTimers.size()) - 1; i >= 0; --i) {
-            if (mTimers[i]->elapsed >= mTimers[i]->duration) {
+            if (mTimers[i]->elapsed >= mTimers[i]->duration && mTimers[i]->removable) {
                 delete mTimers[i];
                 mTimers.erase(mTimers.begin() + i);
             }
@@ -47,6 +48,23 @@ public:
         struct Timer* newTimer = new Timer(
             nextTimerId,
             duration,
+            true,
+            callback
+        );
+
+        nextTimerId++;
+
+        mTimers.push_back(newTimer);
+
+        return newTimer;
+    }
+
+    // take care to avoid memory overloads with this timer
+    Timer *AddNotRemovableTimer(float duration, std::function<void()> callback) {
+        struct Timer* newTimer = new Timer(
+            nextTimerId,
+            duration,
+            false,
             callback
         );
 
@@ -64,6 +82,15 @@ public:
             }
         }
         return 0.f;
+    }
+
+    void Restart(Timer *timer) {
+        for (const auto& t : mTimers) {
+            if (t->id == timer->id) {
+                t->elapsed = 0.f;
+                return;
+            }
+        }
     }
 
 protected:
