5bddd508a9dfc632902430a3098d56b91a824cb7
pedromeerkat <pedro@meerkatcoding.com>
2026-01-26 19:41:59 -0300

    feat: enhance DrawSpriteComponent to include offset parameter for improved sprite positioning

--- src/components/draw/DrawSpriteComponent.cpp
+++ src/components/draw/DrawSpriteComponent.cpp
@@ -6,11 +6,17 @@
 #include "../../actors/Actor.h"
 #include "../../core/Game.h"
 
-DrawSpriteComponent::DrawSpriteComponent(class Actor* owner, const std::string &texturePath, const int width, const int height, const int drawOrder)
-        :DrawComponent(owner, drawOrder)
-        ,mWidth(width)
-        ,mHeight(height)
-        ,mFlip(false)
+DrawSpriteComponent::DrawSpriteComponent(
+    class Actor* owner, 
+    const std::string &texturePath, 
+    const int width, const int height, 
+    const int drawOrder,
+    const Vector2& offset
+):  DrawComponent(owner, drawOrder)
+    ,mWidth(width)
+    ,mHeight(height)
+    ,mFlip(false)
+    ,mOffset(offset)
 {
     mSpriteSheetSurface = mOwner->GetGame()->LoadTexture(texturePath);
 }
@@ -29,6 +35,13 @@ void DrawSpriteComponent::Draw(SDL_Renderer *renderer, const Vector3 &modColor)
 {
     Vector2 cameraPos = mOwner->GetGame()->GetCameraPos();
 
+    SDL_Rect srcrect = {
+        mOffset.x, 
+        mOffset.y,
+        mWidth,
+        mHeight
+    };
+
     SDL_Rect dstrect = {
         static_cast<int>(mOwner->GetPosition().x - mOwner->GetGame()->GetCameraPos().x),
         static_cast<int>(mOwner->GetPosition().y - mOwner->GetGame()->GetCameraPos().y),
@@ -39,8 +52,8 @@ void DrawSpriteComponent::Draw(SDL_Renderer *renderer, const Vector3 &modColor)
     SDL_Point center;
     center.x = static_cast<int>(mPivot.x * mWidth);
     center.y = static_cast<int>(mPivot.y * mHeight);
-    double rotationDeg = mOwner->GetRotation() * 180.0 / Math::Pi;
+    double rotationDeg = Math::ToDegrees(mOwner->GetRotation());
 
     SDL_RendererFlip flip = mFlip ? SDL_FLIP_VERTICAL  : SDL_FLIP_NONE;
-    SDL_RenderCopyEx(renderer, mSpriteSheetSurface, nullptr, &dstrect, rotationDeg, &center, flip);
+    SDL_RenderCopyEx(renderer, mSpriteSheetSurface, &srcrect, &dstrect, rotationDeg, &center, flip);
 }
--- src/components/draw/DrawSpriteComponent.h
+++ src/components/draw/DrawSpriteComponent.h
@@ -10,7 +10,13 @@ class DrawSpriteComponent : public DrawComponent
 {
 public:
     // (Lower draw order corresponds with further back)
-    DrawSpriteComponent(class Actor* owner, const std::string &texturePath, int width = 0, int height = 0, int drawOrder = 100);
+    DrawSpriteComponent(
+        class Actor* owner, 
+        const std::string &texturePath, 
+        int width = 0, int height = 0, 
+        int drawOrder = 100, 
+        const Vector2& offset=Vector2()
+    );
     ~DrawSpriteComponent() override;
 
     void Draw(SDL_Renderer* renderer, const Vector3 &modColor = Color::White) override;
@@ -24,4 +30,5 @@ protected:
     int mWidth;
     int mHeight;
     bool mFlip;
+    Vector2 mOffset;
 };
