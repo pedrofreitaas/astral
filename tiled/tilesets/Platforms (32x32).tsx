71d5ca61035e6b47761f7766795640ec0c8e7bc7
Pedro <freitaas.pedro@gmail.com>
2026-04-22 22:58:14 -0300

    fix: update object positions and dimensions in Platforms (32x32) tileset

--- tiled/tilesets/Platforms (32x32).tsx
+++ tiled/tilesets/Platforms (32x32).tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Platforms (32x32)" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <editorsettings>
+  <export target="../../assets/Levels/Tilesets/Platforms (32x32).json" format="json"/>
+ </editorsettings>
  <image source="Platforms (32x32).png" width="192" height="128"/>
  <tile id="7">
   <objectgroup draworder="index" id="2">
@@ -23,17 +26,17 @@
  </tile>
  <tile id="13">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="11"/>
+   <object id="1" x="0" y="0.0320629" width="32" height="11.9679"/>
   </objectgroup>
  </tile>
  <tile id="14">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="9"/>
+   <object id="1" x="0" y="-0.0969954" width="32" height="10.097"/>
   </objectgroup>
  </tile>
  <tile id="15">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="11"/>
+   <object id="1" x="0" y="0.0965921" width="32" height="11.9034"/>
   </objectgroup>
  </tile>
  <tile id="16">
