dc0e5cc7aca6a5523c039c11aa6f4030634f8a78
pedrofreitaas <freitaas.pedro@gmail.com>
2026-05-20 23:07:05 +0000

    Deploying to gh-pages from @ pedrofreitaas/astral@bbfca1f5b3248c4db43abfede711beccbcd3ad09 🚀

---
+++ CMakeFiles/3.31.6/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.17.0-1013-azure")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.17.0-1013-azure")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/runner/work/_temp/9403c912-2d44-42c4-ac66-ff2dfec74b5f/emsdk-main/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+
+set(CMAKE_SYSTEM "Emscripten-1")
+set(CMAKE_SYSTEM_NAME "Emscripten")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
